7e7bc0caf8f282fb5b3571d65a0e5ee424f5acb5
muhammadazishusein <muhammadazishusein@gmail.com>
2021-11-23 20:05:05 +0700

    first commit

---
+++ resources/views/soal-3.blade.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Soal 2</title>
+
+        <!-- Boostrap -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+        <!-- Styles -->
+        <style>
+            body {
+                font-family: 'Nunito', sans-serif;
+            }
+
+            .cd-seconds {
+                text-align: center;
+                margin: 5vh auto;
+                padding: 15px;
+                width: 50px;
+                border: 1px #000 solid;
+            }
+
+            .question {
+                margin: 5vh auto;
+                width: 200px;
+            }
+
+            .option {
+                display: inline;
+                margin-left: 30px;
+            }
+
+            .next {
+                float: right;
+                margin: 25px;
+            }
+        </style>
+    </head>
+    <body class="antialiased">
+        <div id="cd-seconds" class="cd-seconds"></div>
+
+        <div class="question">
+            <h1>5 / 5 = ...</h1>
+            
+            <div class="option">
+                <input type="radio" name="a" id="a">
+                <label for="a">A. 0</label>
+            </div>
+
+            <div class="option">
+                <input type="radio" name="b" id="b">
+                <label for="b">B. 1</label>
+            </div>
+        </div>
+
+        <a href="/finish" class="next">Selanjutnya</a>
+
+        <!-- Scripts -->        
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"> </script>
+        <script>
+            let timer = function () {
+                let seconds = 30;
+                setInterval(function () {
+                    if (seconds-- == 0) {
+                        window.location.href = "/finish";
+                    }
+                    document.getElementById('cd-seconds').innerHTML = seconds;
+                }, 1000);
+            }
+
+            timer();
+        </script>
+    </body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/soal-1', function () {
+    return view('soal-1');
+});
+
+Route::get('/soal-2', function () {
+    return view('soal-2');
+});
+
+Route::get('/soal-3', function () {
+    return view('soal-3');
+});
+
+Route::get('/finish', function () {
+    return view('finish');
+});
